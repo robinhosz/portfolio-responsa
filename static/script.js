cc4c0cf3abd75b31c903c0febe8a4bea907e6df2
José <joserobsonsiqueira23@hotmail.com>
2021-11-14 03:30:12 -0300

    portfolio att

--- static/script.js
+++ static/script.js
@@ -1,62 +1,62 @@
-const menuMobile = document.querySelector('.menu-mobile');
-const body = document.querySelector('body');
+/* Abre e fecha menu lateral em modo mobile */
 
-menuMobile.addEventListener('click', () => {
-    menuMobile.classList.contains("bi-list")
-        ? menuMobile.classList.replace("bi-list", "bi-x")
-        : menuMobile.classList.replace("bi-x", "bi-list");
-    body.classList.toggle("menu-nav-active");
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
+
+menuMobile.addEventListener("click", () => {
+  menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list");
+  body.classList.toggle("menu-nav-active");
 });
 
+/* Fecha o menu quando clicar em algum item e muda o icone para list */
 
-/*  fecha */
+const navItem = document.querySelectorAll(".nav-item");
 
-const navItem = document.querySelectorAll('.nav-item')
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active");
+      menuMobile.classList.replace("bi-x", "bi-list");
+    }
+  });
+});
 
-navItem.forEach(item => {
-    item.addEventListener("click", () => {
-        if (body.classList.contains("menu-nav-active")) {
-            body.classList.remove("menu-nav-active")
-            menuMobile.classList.replace("bi-x", "bi-list");
-        }
-    })
-
-})
-
-// Animar atributos da tela que tiver data-anime
+// Animar todos os itens na tela que tiverem meu atributo data-anime
 
 const item = document.querySelectorAll("[data-anime]");
 
 const animeScroll = () => {
-    const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
-    
-    item.forEach((element) => {
-        if (windowTop > element.offsetTop) {
-            element.classList.add("animate");
-        }   else {
-            element.classList.remove("animate");
-        }
-    });
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
 };
 
 animeScroll();
 
 window.addEventListener("scroll", ()=>{
-    animeScroll();
+  animeScroll();
 })
 
-// Ativar o carregamento do botão
+// Ativar carregamento no botão de enviar formulário para
 
 const btnEnviar = document.querySelector('#btn-enviar')
 const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
 
 btnEnviar.addEventListener("click", ()=>{
-    btnEnviarLoader.style.display = "block";
-    btnEnviar.style.display = "none"
+  btnEnviarLoader.style.display = "block";
+  btnEnviar.style.display = "none"
 })
 
-// TIrar a mensagem depois de 5 segundos
+// Tira a mensagem de sucesso depois de 5 segundos
 
-setTimeout(() =>{
-    document.querySelector('#alerta').style.display = 'none';
+setTimeout(() => {
+  document.querySelector('#alerta').style.display = 'none';
 }, 5000)
