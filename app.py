9863aa2d972367678ac6dc381faa08a4d9974246
Robson <joserobsonsiqueira23@hotmail.com>
2021-12-27 22:45:45 -0300

    troca de email

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 app.secret_key = 'thicode'
 
 mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
+    "MAIL_SERVER": 'smtp.live.com',
+    "MAIL_PORT": 25,
+    "MAIL_USE_TLS": True,
+    "MAIL_USE_SSL": False,
     "MAIL_USERNAME": os.getenv("EMAIL"),
     "MAIL_PASSWORD": os.getenv("SENHA")
 }
